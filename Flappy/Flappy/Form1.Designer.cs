1cc823750ac55b0c1daed4ee3edd4057b6692f83
Jesus Antonio Sanchez Santos <80359412+antsz25@users.noreply.github.com>
2022-06-06 08:45:08 -0700

    Add files via upload

---
+++ Flappy/Flappy/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Flappy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.upipeline = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.dpipeline = new System.Windows.Forms.PictureBox();
+            this.pbbase = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.lbscore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.upipeline)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dpipeline)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbbase)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // upipeline
+            // 
+            this.upipeline.BackColor = System.Drawing.Color.Transparent;
+            this.upipeline.Image = global::Flappy.Properties.Resources.tubo_arriba;
+            this.upipeline.Location = new System.Drawing.Point(180, 296);
+            this.upipeline.Name = "upipeline";
+            this.upipeline.Size = new System.Drawing.Size(100, 250);
+            this.upipeline.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.upipeline.TabIndex = 0;
+            this.upipeline.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.BackColor = System.Drawing.Color.Transparent;
+            this.bird.Image = global::Flappy.Properties.Resources.bird;
+            this.bird.Location = new System.Drawing.Point(45, 225);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(41, 36);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 1;
+            this.bird.TabStop = false;
+            // 
+            // dpipeline
+            // 
+            this.dpipeline.BackColor = System.Drawing.Color.Transparent;
+            this.dpipeline.Image = global::Flappy.Properties.Resources.tubo_abajo;
+            this.dpipeline.Location = new System.Drawing.Point(192, -89);
+            this.dpipeline.Name = "dpipeline";
+            this.dpipeline.Size = new System.Drawing.Size(100, 301);
+            this.dpipeline.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dpipeline.TabIndex = 2;
+            this.dpipeline.TabStop = false;
+            // 
+            // pbbase
+            // 
+            this.pbbase.BackColor = System.Drawing.Color.Transparent;
+            this.pbbase.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbbase.Image = global::Flappy.Properties.Resources._base;
+            this.pbbase.Location = new System.Drawing.Point(-274, 443);
+            this.pbbase.Name = "pbbase";
+            this.pbbase.Size = new System.Drawing.Size(779, 30);
+            this.pbbase.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbbase.TabIndex = 3;
+            this.pbbase.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1;
+            // 
+            // lbscore
+            // 
+            this.lbscore.AutoSize = true;
+            this.lbscore.BackColor = System.Drawing.Color.Transparent;
+            this.lbscore.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbscore.Location = new System.Drawing.Point(52, 198);
+            this.lbscore.Name = "lbscore";
+            this.lbscore.Size = new System.Drawing.Size(27, 25);
+            this.lbscore.TabIndex = 4;
+            this.lbscore.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Flappy.Properties.Resources.Fondo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(292, 464);
+            this.Controls.Add(this.lbscore);
+            this.Controls.Add(this.pbbase);
+            this.Controls.Add(this.dpipeline);
+            this.Controls.Add(this.bird);
+            this.Controls.Add(this.upipeline);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.upipeline)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dpipeline)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbbase)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox upipeline;
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox dpipeline;
+        private System.Windows.Forms.PictureBox pbbase;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label lbscore;
+    }
+}
+
---
+++ Flappy/Flappy/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy
+{
+    public partial class Form1 : Form
+    {
+        int movcount = 1;
+        bool flyup = false;
+        int distance = 0;
+        bool gamestarted = false;
+        Random position = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        void startGame()
+        {
+            if (gamestarted)
+            {
+                bird.Location = new Point(19, 225);
+                distance = position.Next(-140, 118);
+                dpipeline.Location = new Point(270, -140 - distance);
+                upipeline.Location = new Point(270, 270 - distance);
+                lbscore.Text = "0";
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int movquantity = 5;
+            if (movcount <= movquantity)
+            {
+                bird.Image = Properties.Resources.birdmovu;
+                movcount++;
+            }
+            if ((movcount > movquantity / 2) && (movcount <= movquantity * 2))
+            {
+                bird.Image = Properties.Resources.birdmovd;
+                movcount++;
+            }
+            movcount = (movcount > movquantity * 2) ? 0 : movcount;
+            int y = bird.Location.Y;
+            int x = bird.Location.X;
+            if (flyup)
+            {
+                y = y - 30;
+                flyup = false;
+            }
+            else
+            {
+                y+=2;
+            }
+            bird.Location = new Point(bird.Location.X, y);
+
+            if (bird.Bounds.IntersectsWith(pbbase.Bounds) || bird.Bounds.IntersectsWith(upipeline.Bounds) || bird.Bounds.IntersectsWith(dpipeline.Bounds))
+            {
+                gamestarted = false;
+                timer1.Stop();
+                timer2.Stop();
+                timer3.Stop();
+            }
+            lbscore.Location = new Point(bird.Location.X+10, bird.Location.Y-25);
+            lbscore.Text = (dpipeline.Location.X == bird.Location.X) ? Convert.ToString((Convert.ToInt32(lbscore.Text) + 1)).ToString() : lbscore.Text;
+        
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Space))
+            {
+                if (!gamestarted)
+                {
+                    gamestarted = true;
+                    startGame();
+                    timer1.Start();
+                    timer2.Start();
+                    timer3.Start();
+                }
+                flyup = true;
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(dpipeline.Location.X > -50)
+            {
+                dpipeline.Location = new Point(dpipeline.Location.X - 1, dpipeline.Location.Y);
+                upipeline.Location = new Point(upipeline.Location.X - 1, upipeline.Location.Y);
+            }
+            else
+            {
+                distance = position.Next(-140, 118);
+                dpipeline.Location = new Point(250, -140+distance);
+                upipeline.Location = new Point(250, 270 + distance);
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            pbbase.Location = (pbbase.Location.X > -400) ? new Point(pbbase.Location.X - 1, pbbase.Location.Y) : pbbase.Location = new Point(-9, pbbase.Location.Y);
+        }
+    }
+}
